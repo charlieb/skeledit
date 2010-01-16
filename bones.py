7abb0e301386bf52b1779996c65b8bc09ed2769f
Charlie Burrows <charlie.burrows@gmail.com>
2010-01-16 00:04:48 -0500

    Fixed design of bones

--- bones.py
+++ bones.py
@@ -2,9 +2,12 @@ from matrix import Identity, Rotation, Translation, Scale, Vector
 from math import pi
 
 class Joint:
+    # Joints hold position and transformation relative to the root
+    # This is considered ABSOLUTE
     def __init__(self):
         self.bones = []
         self.transform = Identity()
+        self.position = Vector(0, 0)        
 
     def add_bone(self, bone):
         self.bones.append(bone)
@@ -15,22 +18,25 @@ class Joint:
     def calc_skeleton(self):
         for bone in self.bones:
             bone.calc_transform()
-            bone.joint_to.transform = bone.transform * self.transform
+            bone.calc_end_position()
+            bone.joint_to.transform = self.transform * bone.transform
             bone.joint_to.calc_skeleton()
 
 class Root(Joint):
     def __init__(self):
         Joint.__init__(self)
-        self.position = Vector(0, 0)
+        self.origin = Vector(0, 0)
 
     def __repr__(self):
-        return "Root:\n" + str(self.position)
+        return "Root:\n" + str(self.position)        
 
 class Bone:
+    # Bones hold transformation information from start to end
+    # This is considered RELATIVE
     def __init__(self, joint_from):
         joint_from.add_bone(self)
-        self.joint_from = joint_from
-        self.joint_to = Joint()
+        self.start = joint_from
+        self.end = Joint()
         self.rotation = 0
         self.length = 1
         self.transform = Identity()
@@ -45,6 +51,10 @@ class Bone:
             Translation(Vector(0, self.length))
         print "calc_transform"
         print self.transform
+
+    def calc_end_position(self):
+        # You probably want to call calc_transform first!
+        self.end.position = self.start.position * self.transform
 
 def print_skeleton(root):
     print root
