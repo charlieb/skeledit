7b4de9dbc79ca7d03e3b57d6e3f94bbd32372098
Charlie Burrows <charlie.burrows@gmail.com>
2010-01-27 21:39:17 -0500

    Fixed annoying pygame event spam performance issue

--- src/main.py
+++ src/main.py
@@ -14,13 +14,24 @@ def main():
     print '\nAdd a bone: select a joint and press (n)ew'
     print '\nDelete a bone: select a bone and press (d)elete'
 
+    def remove_dups(event_list):
+        last_event_type = None
+        remaining_events = []
+        for e in event_list:
+            if e.type != last_event_type:
+                remaining_events.append(e)
+                last_event_type = e.type
+        #print "%s / %s"%(len(remaining_events), len(event_list))
+        return remaining_events
+
     mouse_down = False
     while True:
         pygame.event.pump()
-        event = pygame.event.poll()
+        events = remove_dups(pygame.event.get())        
 
         # when event returns False it's time to go
-        if not ui.event(event): return
+        for event in events:
+            if not ui.event(event): return
         ui.update()
         ui.draw(screen)
         
